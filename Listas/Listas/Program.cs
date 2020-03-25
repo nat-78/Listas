5063b98579f4e5b448e9aea1d5312fc34c26155a
nat-78 <53804974+nat-78@users.noreply.github.com>
2020-03-25 17:03:57 -0300

    Terminado de manera diferente a lo que se pidio aunque el resultado es el mismo

---
+++ Listas/Listas/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas
+{
+    class Program
+    {
+
+        private static readonly string[] colors = { "MAGENTA", "RED", "WHITE", "BLUE", "CYAN" };
+        private static readonly string[] removeColors = { "RED", "WHITE", "BLUE" };
+
+        static void Main(string[] args)
+        {
+
+            List<string> colors = new List<string>();
+            colors.Add("MAGENTA");
+            colors.Add("RED");
+            colors.Add("WHITE");
+            colors.Add("BLUE");
+            colors.Add("CYAN");
+
+
+
+            List<string> removecolors = new List<string>();
+            removecolors.Add("RED");
+            removecolors.Add("WHITE");
+            removecolors.Add("BLUE");
+            foreach (string color in colors)
+
+                Console.WriteLine(color);
+
+            colors.Remove("RED");
+            colors.Remove("WHITE");
+            colors.Remove("BLUE");
+
+
+
+            foreach (string colores in colors)
+            
+                Console.WriteLine(colores);
+            
+           
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+        
+      
+
+    }
+
+  
+   
+    }
